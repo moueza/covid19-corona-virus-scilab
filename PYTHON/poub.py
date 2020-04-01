d0d731fa04732e1ce2c6ee3a14a1225c885b0ed9
moueza <mouezapeter@gmail.com>
2020-04-02 00:35:09 +0200

    python

--- PYTHON/poub.py
+++ PYTHON/poub.py
@@ -4,6 +4,7 @@ with open('../time_series_covid19_deaths_global.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
+        row
         #if line_count == 0:
         #    print(f'Column names are {", ".join(row)}')
         #    line_count += 1
@@ -11,5 +12,5 @@ with open('../time_series_covid19_deaths_global.csv') as csv_file:
         #    print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
         #    line_count += 1
 		#line[1]=row
-		print row
+		
     #print(f'Processed {line_count} lines.')

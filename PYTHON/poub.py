2a32186c24a813187a0415c5bed48120a56c6efc
moueza <>
2020-04-08 11:54:10 +0200

    See #5 python : sub matrix and integer conversion

--- PYTHON/poub.py
+++ PYTHON/poub.py
@@ -1,3 +1,4 @@
+
 #file mag Login pratique blanc no 10 p23 :
 #                                 classes p14
 #         filtres listes p18
@@ -70,5 +71,36 @@ with open('../time_series_covid19_deaths_global.csv') as csv_file:
       
     #print(f'Processed {line_count} lines.')
     print(arrayMoi[0])
-    #TODO convert to np.array https://docs.scipy.org/doc/numpy/reference/generated/numpy.array.html#numpy.array
+    print('\nlbl45')
+    print(arrayMoi)
+    print('\nlbl50')
+    #print(arrayMoi[:][4:]
+    arrayMoiArray=np.asarray(arrayMoi)
+    arrayMoiMatrix=np.matrix(arrayMoi)
+#TODO convert to np.array https://docs.scipy.org/doc/numpy/reference/generated/numpy.array.html#numpy.array
+    subArray=arrayMoiArray[0:1000][4:1000] #KO
+    subMatrix=arrayMoiMatrix[0:1000][4:1000]# KO rien
+
+    #subArray2
+    subMatrix2=arrayMoiMatrix[:,4:]
     
+    subArray.flatten()
+    subMatrix.flatten()
+
+    #https://docs.scipy.org/doc/numpy/reference/generated/numpy.vectorize.html
+    def myfunc(x):
+        "Return a-b if a>b, otherwise return a+b"
+        return int(x)
+    vfunc = np.vectorize(myfunc)
+ 
+    #vfunc(subArray, 2)
+    #vfunc(subArray) #KO rien finalement
+    #vfunc(subMatrix, 2)
+    #vfunc(subMatrix) #KO rien finalement
+    subMatInt2=vfunc(subMatrix2)
+    
+    #affich array en ligne
+    a = np.array([[ 0,  1,  2,  3], [10, 11, 12, 13],[20, 21, 22, 23], [30, 31, 32, 33], [40, 41, 42, 43]])
+    # a[:][1]KO
+    m=np.matrix(a)
+    #TODO col des 4 par append

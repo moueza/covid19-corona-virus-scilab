82b35962047cb108f10f70b5d22f2b7c830c84ab
moueza <>
2021-03-06 04:14:30 +0100

    see #31 bean def

---
+++ JAVA/covid19-corona-virus-scilab/src/main/java/conf/DataSourceConfiguration.java
@@ -0,0 +1,11 @@
+package conf;
+
+/**
+ * https://docs.spring.io/spring-javaconfig/docs/1.0.0.m3/reference/html/creating-bean-definitions.html
+ * 
+ * https://docs.spring.io/spring-framework/docs/4.2.x/spring-framework-reference/html/
+ */
+@Configuration(defaultAutowire = Autowire.BY_TYPE, defaultLazy = Lazy.FALSE)
+public class DataSourceConfiguration {
+	// bean definitions follow
+}
